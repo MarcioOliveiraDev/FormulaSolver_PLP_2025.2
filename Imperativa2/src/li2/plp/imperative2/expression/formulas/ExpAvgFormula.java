fe972c3174faf5e7effd06f9137386d66ec21d34
Márcio Brito <marcioyoung90@gmail.com>
2025-11-25 20:56:52 -0300

    Implementação da função Average -> AVG substituindo função MEDIAN

--- Imperativa2/src/li2/plp/imperative2/expression/formulas/ExpAvgFormula.java
+++ Imperativa2/src/li2/plp/imperative2/expression/formulas/ExpAvgFormula.java
@@ -1,9 +1,5 @@
 package li2.plp.imperative2.expression.formulas;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import li2.plp.expressions1.util.Tipo;
 import li2.plp.expressions1.util.TipoPrimitivo;
 import li2.plp.expressions2.expression.Expressao;
@@ -15,55 +11,54 @@ import li2.plp.expressions2.memory.VariavelJaDeclaradaException;
 import li2.plp.expressions2.memory.VariavelNaoDeclaradaException;
 import li2.plp.imperative2.command.ListaExpressao;
 
-public class ExpMedianFormula extends ExpFormulaMultipleArgs {
+public class ExpAvgFormula extends ExpFormulaMultipleArgs {
 
-    public ExpMedianFormula(ListaExpressao args) {
+    public ExpAvgFormula(ListaExpressao args) {
         super(args);
     }
 
     @Override
     public Valor avaliar(AmbienteExecucao amb) throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
-        if (args == null || args.getHead() == null) return new ValorInteiro(0);
+        // Se a lista for nula ou vazia, retorna 0 para evitar divisão por zero
+        if (args == null || args.getHead() == null) {
+             return new ValorInteiro(0);
+        }
+        
+        int soma = 0;
+        int contador = 0;
 
-        List<Integer> valores = new ArrayList<Integer>();
         for (Expressao expressao : args) {
             Valor val = expressao.avaliar(amb);
-            valores.add(((ValorInteiro) val).valor());
+            soma += ((ValorInteiro) val).valor();
+            contador++;
         }
-
-        Collections.sort(valores);
-
-        int n = valores.size();
-        int mediana;
-
-        if (n % 2 == 1) {
-            // Ímpar: pega o do meio
-            mediana = valores.get(n / 2);
-        } else {
-            // Par: média dos dois do meio (divisão inteira)
-            int v1 = valores.get((n / 2) - 1);
-            int v2 = valores.get(n / 2);
-            mediana = (v1 + v2) / 2;
-        }
-
-        return new ValorInteiro(mediana);
+        
+        // Divisão Inteira (ex: média de 10 e 3 será 6, não 6.5)
+        return new ValorInteiro(soma / contador);
     }
 
     @Override
     public boolean checaTipo(AmbienteCompilacao amb) throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
         for (Expressao expressao : args) {
-            if (!expressao.getTipo(amb).eInteiro()) return false;
+            if (!expressao.getTipo(amb).eInteiro()) {
+                return false;
+            }
         }
         return true;
     }
 
     @Override
-    public Tipo getTipo(AmbienteCompilacao amb) {
+    public Tipo getTipo(AmbienteCompilacao amb) throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
         return TipoPrimitivo.INTEIRO;
     }
 
     @Override
+    public Expressao reduzir(AmbienteExecucao ambiente) {
+        return this;
+    }
+
+    @Override
     public Expressao clone() {
-        return new ExpMedianFormula(args);
+        return new ExpAvgFormula(args);
     }
 }
